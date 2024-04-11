5ec38b7d8819be0053a21cf43c530fd4b55d2d2d
Diego Filgueiras <diego.filgueiras.084@ufrn.edu.br>
2024-04-11 00:17:09 -0300

    feat(back): adiciona classe DTO de usuário novamente

---
+++ server/RecipeHub/src/main/java/org/example/recipehub/model/dto/UsuarioDTO.java
@@ -0,0 +1,73 @@
+package org.example.recipehub.model.dto;
+
+import org.example.recipehub.model.Usuario;
+
+public class UsuarioDTO {
+
+    private Long id;
+    private String nome;
+    private String login;
+    private String senha;
+    private String email;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Long id, String nome, String login, String senha, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public UsuarioDTO(Usuario usuario) {
+        if (usuario != null) {
+            id = usuario.getId();
+            nome = usuario.getNome();
+            login = usuario.getLogin();
+            senha = usuario.getSenha();
+            email = usuario.getEmail();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
